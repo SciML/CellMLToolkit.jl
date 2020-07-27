1ef2049889a521af069ff2298ca993d4cd8ae20d
Shahriar Iravanian <siravan@emory.edu>
2020-07-26 23:20:21 -0400

    files added

---
+++ test/runtests.jl
@@ -0,0 +1,27 @@
+using Test
+using CellMLToolkit
+using OrdinaryDiffEq
+
+ml = CellModel("../models/beeler_reuter_1977.cellml.xml")
+
+@test length(ml.eqs) == 8
+@test ml.iv.op.name == :time
+
+eqs, vs = CellMLToolkit.flat_equations(ml)
+@test length(vs) == 8
+
+@test find_V(ml).op.name == :V
+
+prob = ODEProblem(ml, (0,10000.0))
+sol1 = solve(prob, Euler(), dt=0.01, saveat=1.0)
+sol2 = solve(prob, TRBDF2(), dtmax=0.5, saveat=1.0)
+V1 = map(x -> x[1], sol1.u)
+V2 = map(x -> x[1], sol2.u)
+err = sum(abs.(V1 .- V2)) / length(V1)
+@test err < 0.1
+
+prob = ODEProblem(ml, (0,10000.0); jac=true)
+sol3 = solve(prob, TRBDF2(), dtmax=0.5, saveat=1.0)
+V3 = map(x -> x[1], sol2.u)
+err = sum(abs.(V1 .- V3)) / length(V1)
+@test err < 0.1
