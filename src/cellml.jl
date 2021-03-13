1b765d2da6d3498c9e386e36824328421126ae9c
Shahriar Iravanian <siravan@svtsim.com>
2021-03-13 15:22:33 -0500

    list_p&u0

--- src/cellml.jl
+++ src/cellml.jl
@@ -1,7 +1,15 @@
-using EzXML
+using EzXML, Symbolics, Statistics
+using Symbolics: FnType, Sym
+
+
+include("parse.jl")
+include("utils.jl")
 
 const cellml_ns = "http://www.cellml.org/cellml/1.0#"
 const mathml_ns = "http://www.w3.org/1998/Math/MathML"
+
+create_sym(x) = Num(Variable(Symbol(x)))
+create_sym(x, iv) = Num(Sym{FnType{Tuple{Real}}}(Symbol(x))(Variable(Symbol(iv))))
 
 # list the name of the CellML variables (state variables + parameters)
 function list_variables(xml)
@@ -35,4 +43,31 @@ function list_parameters(xml)
     iv = find_iv(xml)
     params = filter(x -> x ∉ states && x != iv, vars)
     return params
+end
+
+# create_sym(x::AbstractString) = Num(Variable(Symbol(x)))
+# create_sym(x::AbstractString, iv::AbstractString) = Sym{FnType{Tuple{Real}}}(Symbol(x))(Symbol(iv))
+
+function list_p(xml)
+    vars = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
+    params = list_parameters(xml)
+    p = map(x -> (create_sym(x["name"]) => x["initial_value"]), filter(x -> x["name"] ∈ params, vars))
+    return p
+end
+
+function list_u0(xml)
+    vars = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
+    states = list_state_variables(xml)
+    iv = find_iv(xml)
+    # u0 = map(x -> (create_sym(x["name"], iv) => x["initial_value"]), filter(x -> x["name"] ∈ states, vars))
+    u0 = map(x -> (create_sym(x["name"]) => x["initial_value"]), filter(x -> x["name"] ∈ states, vars))
+    return u0
+end
+
+function list_substitution(xml)
+    vars = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
+    states = list_state_variables(xml)
+    iv = find_iv(xml)
+    u0 = map(x -> (create_sym(x["name"]) => create_sym(x["name"], iv)), filter(x -> x["name"] ∈ states, vars))
+    return u0
 end
