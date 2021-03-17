b5180ba16f43ac85b3b769d177a6349741a34cb5
Shahriar Iravanian <siravan@svtsim.com>
2021-03-17 17:42:19 -0400

    CellMLToolkit.jl is updated for ver 2.0

--- src/CellMLToolkit.jl
+++ src/CellMLToolkit.jl
@@ -1,550 +1,78 @@
-# module CellMLToolkit
+module CellMLToolkit
 
-export CellModel, ODEProblem
-export parse_file, process_doc
-export find_adjacency_matrix, find_V
-export list_params, list_initial_conditions, list_states, update_list!
+using EzXML #, Symbolics, SymbolicUtils
+using SymbolicUtils: FnType, Sym, operation
 
-using LightXML
+#using MathML
+using Statistics
+include("parse.jl")
+include("utils.jl")
+
 using ModelingToolkit
 
-import Base.floor, Base.ceil
+include("cellml.jl")
 
-const T = Float64
-
-# The Heavide function
-H(x) = (x >= zero(x) ? one(x) : zero(x))    # 1 / (1 + exp(-10000*x))
-ModelingToolkit.@register H(x)
-const σ = T(1e-4)
-ModelingToolkit.derivative(::typeof(H), args::NTuple{1,Any}, ::Val{1}) = 1/(sqrt(2π)*σ)*exp(-args[1]^2/(2σ)^2)  # Dirac δ
-
-ModelingToolkit.@register floor(x)
-ModelingToolkit.@register ceil(x)
-ModelingToolkit.@register round(x)
-ModelingToolkit.derivative(::typeof(floor), args::NTuple{1,Any}, ::Val{1}) = zero(args[1])
-ModelingToolkit.derivative(::typeof(ceil), args::NTuple{1,Any}, ::Val{1}) = zero(args[1])
-ModelingToolkit.derivative(::typeof(round), args::NTuple{1,Any}, ::Val{1}) = zero(args[1])
-
-# children(c) = collect(child_elements(c))
-# first_child(c) = first(child_elements(c))
-
-parent = Dict{XMLElement, XMLElement}()
-
-function children(e)
-    l = collect(child_elements(e))
-    for c in l
-        parent[c] = e
-    end
-    return l
-end
-
-function first_child(e)
-    c = first(child_elements(e))
-    parent[c] = e
-    return c
-end
-
-function print_history(e)
-    println("XML context")
-    level = 1
-    while haskey(parent, e) && level < 3
-        println(level, ": ----------------------------------------------")
-        println(e)
-        e = parent[e]
-        level += 1
-    end
-end
-
-function parse_error(e, msg)
-    print_history(e)
-    error(msg)
-end
-
-#############################################################################
-
-mutable struct CellModel
-    vars::Dict{String, Operation}   # the list of model variables
-    inits::Dict{String, T}          # the initialization list
-    eqs::Array{Equation}            # the list of ODE equations
-    alg::Array{Equation}            # the algebraic equations
-    iv::Union{Operation, Nothing}   # the independent variable
-    units::Dict{Operation, String}  # the variable units
-
-    function CellModel()
-        return new(
-            Dict{String,Operation}(),
-            Dict{String,T}(),
-            Equation[],
-            Equation[],
-            nothing,
-            Dict{Operation,String}()
-        )
-    end
-end
-
-CellModel(doc::XMLDocument; dependency=true) = process_doc(doc; dependency=dependency)
-CellModel(s::AbstractString; dependency=true) = process_doc(parse_file(s); dependency=dependency)
-
-########################### Basic Utility Functions ##########################
-
-function get_var(ml::CellModel, tag)
-    if haskey(ml.vars, tag)
-        return ml.vars[tag]
-    else
-        v = Variable(Symbol(tag))()
-        ml.vars[tag] = v
-        return v
-    end
-end
-
-function list_states(ml::CellModel)
-    states = Set{Operation}()
-    for eq in ml.eqs
-        if eq.lhs.op isa Variable
-            push!(states, eq.lhs)
-        else
-            push!(states, eq.lhs.args[1])
-        end
-    end
-    return states
-end
-
-################### Utility Functions for Adjacency Matrix ####################
-
-function find_dependency_list(u::Operation)
-    if u.op isa Variable
-        v = Set{Operation}([u])
-    else
-        v = Set{Operation}()
-        for w in u.args
-            if w isa Operation
-                v = union(v, find_dependency_list(w))
-            end
-        end
-    end
-    return v
-end
-
-function find_adjacency_matrix(ml::CellModel; level=1)
-    eqs, vs = flat_equations(ml; level=level)
-    n = length(vs)
-    a = zeros(Int, (n,n))
-
-    for i = 1:n
-        l = find_dependency_list(eqs[i].rhs)
-        for j = 1:n
-            if vs[j] ∈ l
-                a[j,i] = 1
-            end
-        end
-    end
-
-    return a, vs
+"""
+    reads a CellML path or io and returns an ODESystem
+"""
+function read_cellml(path)
+    xml = readxml(path)
+    return read_cellml(xml)
 end
 
 """
-    A heuristic algorithm to find the state variable corresponding to
-    the transmembrane potential: it should occured on the dependency
-    list more than any other state variable
+    parses a CellML XML string and returns an ODESystem
 """
-function find_V(ml::CellModel)
-    a, vs = find_adjacency_matrix(ml)
-    u = vec(sum(a, dims=1))
-    V = vs[findmax(u)[2]]
-    return V
+function parse_cellml(xmlstr::AbstractString)
+    xml = parsexml(xmlstr)
+    return read_cellmL(xml)
 end
 
-################################# Tree Traversal ##############################
+##############################################################################
 
-"""
-    the entry point
-"""
-function process_doc(doc; dependency=true)
-    ml = CellModel()
+export CellModel, ODEProblem
+export read_cellml, parse_cellml
+export list_params, list_initial_conditions, list_states, update_list!
+export eval_cellml, eval_string, readxml
 
-    for e in get_elements_by_tagname(root(doc), "component")
-        process_component(ml, e)
-    end
-
-    if dependency
-        states = collect(list_states(ml))
-        map(v -> push!(v.args, ml.iv), states)
-    end
-
-    return ml
-end
-
-function process_component(ml::CellModel, e)
-    name = attribute(e, "name")
-
-    for c in get_elements_by_tagname(e, "variable")
-        process_variable(ml, c)
-    end
-
-    for c in get_elements_by_tagname(e, "math")
-        process_math(ml, c)
-    end
-end
-
-function process_variable(ml::CellModel, e)
-    s = strip(attribute(e, "name"))
-
-    if has_attribute(e, "public_interface")
-        u = strip(attribute(e, "units"))
-        v = get_var(ml, s)
-        ml.units[v] = u
-    end
-
-    if has_attribute(e, "initial_value") && !haskey(ml.inits, s)
-        val = T(parse(Float64, attribute(e, "initial_value")))
-        ml.inits[s] = val
-    end
-end
-
-function process_math(ml::CellModel, e)
-    for c in get_elements_by_tagname(e, "apply")
-        process_math_element(ml, c)
-    end
-end
-
-function process_math_element(ml::CellModel, e)
-    if name(e) != "apply"
-        parse_error(e, "a math clause should be a list of applies")
-    end
-
-    l = children(e)
-
-    if name(l[1]) != "eq"
-        parse_error(e, "a math clause should be an assignment")
-    end
-
-    if name(l[2]) == "ci"
-        tag = strip(content(l[2]))
-        lhs = get_var(ml, tag)
-        rhs = convert_term(ml, l[3])
-        eq = lhs ~ rhs
-        push!(ml.alg, eq)
-    elseif name(l[2]) == "apply"
-        h = children(l[2])
-
-        if name(h[1]) != "diff"
-            parse_error(e, "expected diff")
-        end
-
-        if name(h[2]) == "bvar"
-            x_tag = strip(content(first_child(h[2])))
-        else
-            x_tag = strip(content(h[2]))
-        end
-
-        y_tag = strip(content(h[3]))
-        X = Variable(Symbol(x_tag))()
-        if ml.iv == nothing
-            ml.iv = X
-        elseif ml.iv.op !== X.op
-            parse_error(e, "only can define one independent variable")
-        end
-        Y = get_var(ml, y_tag)
-        D = Differential(X)
-        lhs = D(Y)
-        rhs = convert_term(ml, l[3])
-        eq = lhs ~ rhs
-        push!(ml.eqs, eq)
-    else
-        parse_error(e, "expected an arithmetic or differential assignment")
-    end
-end
-
-function convert_term(ml::CellModel, e)
-    s = strip(name(e))
-
-    if s == "apply"
-        return convert_apply(ml, e)
-    elseif s == "ci"
-        tag = strip(content(e))
-        return get_var(ml, tag)
-    elseif s == "cn"
-        return process_cn(e)
-    elseif s == "piecewise"
-        return convert_piecewise(ml, e)
-    elseif s == "bvar"
-        parse_error(e, "derivatives are unsupported on the right hand side of equations, please use algebraic equations instead: $e")
-    else
-        x = convert_math_constants(e)
-        if x == nothing
-            parse_error(e, "unrecognized term $s: $e")
-        end
-        return x
-    end
-end
-
-function convert_apply(ml::CellModel, e)
-    n = length(children(e))
-
-    if n == 2
-        return convert_unary_apply(ml, e)
-    elseif n == 3
-        return convert_binary_apply(ml, e)
-    else
-        return convert_nary_apply(ml, e)
-    end
-end
-
-function convert_unary_apply(ml::CellModel, e)
-    l = children(e)
-    s = name(l[1])
-    t = convert_term(ml, l[2])
-
-    if s == "plus"
-        return t
-    elseif s == "minus"
-        return -t
-    elseif s == "ln"
-        return log(t)
-    elseif s == "log"
-        return log10(t)
-    elseif s == "sin"
-        return sin(t)
-    elseif s == "cos"
-        return cos(t)
-    elseif s == "tan"
-        return tan(t)
-    elseif s == "arcsin"
-        return asin(t)
-    elseif s == "arccos"
-        return acos(t)
-    elseif s == "arctan"
-        return atan(t)
-    elseif s == "sinh"
-        return sinh(t)
-    elseif s == "cosh"
-        return cosh(t)
-    elseif s == "tanh"
-        return tanh(t)
-    elseif s == "arcsinh"
-        return asinh(t)
-    elseif s == "arccosh"
-        return acosh(t)
-    elseif s == "arctanh"
-        return atanh(t)
-    elseif s == "exp"
-        return exp(t)
-    elseif s == "root"
-        return sqrt(t)
-    elseif s == "abs"
-        return abs(t)
-    elseif s == "floor"
-        return floor(t)
-    elseif s == "ceiling"
-        return ceil(t)
-    elseif s == "not"
-        return one(t) - t
-    else
-        parse_error(e, "unrecognized unary operator: $s")
-    end
-end
-
-function convert_binary_apply(ml::CellModel, e)
-    l = children(e)
-    s = name(l[1])
-    t1 = convert_term(ml, l[2])
-    t2 = convert_term(ml, l[3])
-
-    if s == "diff"
-        return Differential(t1)(t2)
-    end
-
-    if s == "min"
-        return min(t1, t2)
-    elseif s == "max"
-        return max(t1, t2)
-    elseif s == "plus"
-        return t1 + t2
-    elseif s == "minus"
-        return t1 - t2
-    elseif s == "times"
-        return t1 * t2
-    elseif s == "divide"
-        return t1 / t2
-    elseif s == "rem"
-        return t1 % t2
-    elseif s == "power"
-        return t1 ^ t2
-    elseif s == "and"
-        return t1 * t2
-    elseif s == "or"
-        return t1 + t2 - t1 * t2
-    elseif s == "xor"
-        return abs(t1 - t2)
-    elseif s == "leq"
-        return H(t2 - t1)
-    elseif s == "geq"
-        return H(t1 - t2)
-    elseif s == "lt"
-        return H(t2 - t1 - eps(T))
-    elseif s == "gt"
-        return H(t1 - t2 - eps(T))
-    elseif s == "eq"
-        return H(t1 - t2) * H(t2 - t1)
-    elseif s == "neq"
-        return 1 - H(t1 - t2) * H(t2 - t1)
-    else
-        parse_error(e, "unrecognized binary operator: $s")
-    end
-end
-
-function apply_nary_boolean(s, ts, e)
-    if length(ts) == 1
-        return ts[1]
-    elseif s == "and"
-        return ts[1] * apply_nary_boolean(s, ts[2:end])
-    elseif s == "or"
-        tail = apply_nary_boolean(s, ts[2:end])
-        return ts[1] + tail - ts[1] * tail
-    else
-        parse_error(e, "unrecognized nary operator: $s")
-    end
-end
-
-function convert_nary_apply(ml::CellModel, e)
-    l = children(e)
-    s = name(l[1])
-    ts = [convert_term(ml, c) for c in l[2:end]]
-
-    if s == "plus"
-        return Operation(+, ts)
-    elseif s == "times"
-        return Operation(*, ts)
-    else
-        return apply_nary_boolean(s, ts, e)
-    end
-end
-
-const constants = Dict(
-    "pi" => T(π),
-    "exponentiale" => T(ℯ),
-    "notanumber" => NaN,
-    "infinity" => Inf,
-    "true" => true,
-    "false" => false
-)
-
-function convert_math_constants(e)
-    try
-        return constants[name(e)]
-    catch
-        return nothing
-    end
-end
-
-function process_cn(e)
-    if has_attribute(e, "type")
-        l = collect(child_nodes(e))
-        if name(l[2]) != "sep"
-            parse_error(e, "An e-notation number should have <sep>")
-        end
-        s = strip(content(l[1])) * "e" * strip(content(l[3]))
-    else
-        s = content(e)
-    end
-    return parse(T, s)
-end
-
-function convert_piecewise(ml::CellModel, e)
-    l = []
-    for c in children(e)
-        s = name(c)
-        h = children(c)
-        if s == "piece"
-            push!(l, convert_term(ml, h[1]))
-            push!(l, convert_term(ml, h[2]))
-        elseif s == "otherwise"
-            push!(l, convert_term(ml, h[1]))
-        end
-    end
-
-    return generate_piecewise(l)
-end
-
-function generate_piecewise(l)
-    if length(l) == 1
-        return l[1]
-    else
-        # l[2] should be an H expression
-        return l[2]*l[1] + (one(l[2])-l[2])*generate_piecewise(l[3:end])
-    end
-end
-
-#############################################################################
-
-"""
-    flat_equations flattens the equation list by substituting algebraic
-    equations in the differential equations
-"""
-function flat_equations(ml::CellModel; level=1)
-    alg = [a.lhs => a.rhs for a in ml.alg]
-    for i = 1:level
-        alg = [first(a) => substitute(last(a), alg) for a in alg]
-    end
-    eqs = [eq.lhs ~ substitute(eq.rhs, alg) for eq in ml.eqs]
-    vs = [eq.lhs.args[1] for eq in eqs]
-    return eqs, vs
+struct CellModel
+    xml::EzXML.Document
+    sys::ODESystem
 end
 
 """
-    get_init_list returns a list of variable => initial value pairs
-
-    ml is a CellModel
-    if select_params == true, it returns a list of parameters (p in ODEProblem)
-    if select_params == false, it returns a list of state variables (u0 in ODEProblem)
+    constructs a CellModel struct for the CellML model defined in path
 """
-function get_init_list(ml::CellModel, select_params=false)
-    l = Pair{Operation,T}[]
-    states = list_states(ml)
+function CellModel(path::AbstractString; dependency=true)
+    xml = readxml(path)
+    CellModel(xml, read_cellml(xml))
 
-    for v in values(ml.vars)
-        s = repr(v.op)
-        if haskey(ml.inits, s) && (v ∈ states) != select_params
-            push!(l, v => ml.inits[s])
-        end
-    end
-
-    return l
+    # fall-back option:
+    # s = eval_string(xml)
+    # eval(Meta.parse(s))
+    # sys = ODESystem(eqs)
+    # CellModel(xml, sys)
 end
 
-"""
-    list_initial_conditions returns a list of state variable => initial value pairs
-    u0 in ODEProblem
-"""
-list_initial_conditions(ml::CellModel) = get_init_list(ml, false)
+list_states(ml::CellModel) = list_states(ml.xml)
+list_params(ml::CellModel) = list_params(ml.xml)
+# list_initial_conditions(ml::CellModel) = list_initial_conditions(ml.xml)
 
-"""
-    list_params returns a list of params => initial value pairs
-    p in ODEProblem
-"""
-list_params(ml::CellModel) = get_init_list(ml, true)
-
-"""
-    update_list! updates the value of an item in an initial value list (either p or u0)
-    l is the list
-    sym is a Symbol pointing to the variable (we also pass a string or a ModelingToolkit.Operation)
-    val is the new value
-"""
-function update_list!(l::Array{Pair{Operation,Float64}}, sym::Symbol, val)
-    for (i,x) in enumerate(l)
-        if first(x).op.name == sym
-            l[i] = first(x) => T(val)
-            return
-        end
-    end
-    parse_error(e, "param not found: $name")
+# extracts the initial conditions from ml.xml and sorts them
+# according to ml.sys.states
+function list_initial_conditions(ml::CellModel)
+    states = ml.sys.states
+    u0 = list_initial_conditions(ml.xml)
+    # map(x -> (x => value_of(x, u0)), states)
+    map(x -> value_of(x, u0), states)
 end
 
-update_list!(l::Array{Pair{Operation,Float64}}, name::AbstractString, val) =
-        update_list!(l, Symbol(name), val)
+function value_of(x, u0)
+    name = string(x)
+    i = findfirst(x -> string(first(x)) == name, u0)
+    return last(u0[i])
+end
 
-update_list!(l::Array{Pair{Operation,Float64}}, v::Operation, val) =
-        update_list!(l, v.op, val)
 
 import ModelingToolkit.ODEProblem
 
@@ -553,14 +81,25 @@ import ModelingToolkit.ODEProblem
 """
 function ODEProblem(ml::CellModel, tspan;
         jac=false, level=1, p=list_params(ml), u0=list_initial_conditions(ml))
-    eqs, vs = flat_equations(ml; level=level)
-    # u0, p = get_init_lists(ml)
-    ps = map(first, p)
-    sys = ODESystem(eqs, ml.iv, vs, ps)
-    prob = ODEProblem(sys, u0, tspan, p; jac=jac)
-    return prob
+    ODEProblem(ml.sys, u0, tspan, p; jac=jac)
 end
 
-include("generator.jl")
+"""
+    update_list! updates the value of an item in an initial value list (either p or u0)
+    l is the list
+    sym is a Symbol pointing to the variable (we also pass a string)
+    val is the new value
+"""
+function update_list!(l, sym::Symbol, val)
+    for (i,x) in enumerate(l)
+        if first(x).name == sym
+            l[i] = first(x) => val
+            return
+        end
+    end
+    parse_error(e, "param not found: $name")
+end
 
-# end # module
+update_list!(l, name::AbstractString, val) = update_list!(l, Symbol(name), val)
+
+end # module
--- src/cellml.jl
+++ src/cellml.jl
@@ -1,9 +1,3 @@
-using EzXML, Symbolics, Statistics
-using Symbolics: FnType, Sym
-
-include("parse.jl")
-include("utils.jl")
-
 const cellml_ns = "http://www.cellml.org/cellml/1.0#"
 const mathml_ns = "http://www.w3.org/1998/Math/MathML"
 
@@ -11,18 +5,19 @@ create_var(x) = Num(Variable(Symbol(x))).val
 create_var(x, iv) = Num(Sym{FnType{Tuple{Real}}}(Symbol(x))(Variable(Symbol(iv)))).val
 create_param(x) = Num(Sym{ModelingToolkit.Parameter{Real}}(Symbol(x))).val
 
-# list the name of the CellML variables (state variables + parameters)
-function list_variables(xml)
-    nodes = findall("//x:variable/@name", root(xml), ["x"=>cellml_ns])
-    vars = nodecontent.(nodes)
-    return unique(vars)
+# list the name of the CellML initialized variables, i.e., state variables
+# and parameters with an initial_value tag
+function find_initialized_variables(xml)
+    return findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
 end
 
 # list the name of the state variables, i.e., variables that occur on the left
 # hand side of an assignment
-function list_state_variables(xml)
-    nodes = findall("//x:math//x:eq/following-sibling::x:apply[1]/x:ci", root(xml), ["x"=>mathml_ns])
-    vars = nodecontent.(nodes)
+function find_state_names(xml)
+    # nodes = findall("//x:math//x:eq/following-sibling::x:apply[1]/x:ci", root(xml), ["x"=>mathml_ns])
+    # vars = nodecontent.(nodes)
+    nodes = findall("//x:math//x:apply/x:diff", root(xml), ["x"=>mathml_ns])
+    vars = [nodecontent(lastelement(parentnode(n))) for n in nodes]
     return unique(vars)
 end
 
@@ -36,39 +31,35 @@ function find_iv(xml)
     return ivs[1]
 end
 
-# list the name of all the parameters
-function list_parameters(xml)
-    vars = list_variables(xml)
-    states = list_state_variables(xml)
-    iv = find_iv(xml)
-    params = filter(x -> x ∉ states && x != iv, vars)
-    return params
+function list_params(xml, iv=find_iv(xml))
+    vars = find_initialized_variables(xml)
+    states = find_state_names(xml)
+    p = filter(x -> x["name"] ∉ states && x["name"] != iv, vars)
+    map(x -> (create_param(x["name"]) => parse(Float64, x["initial_value"])), p)
 end
 
-# create_var(x::AbstractString) = Num(Variable(Symbol(x)))
-# create_var(x::AbstractString, iv::AbstractString) = Sym{FnType{Tuple{Real}}}(Symbol(x))(Symbol(iv))
-
-function list_p(xml)
-    vars = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
-    params = list_parameters(xml)
-    p = map(x -> (create_param(x["name"]) => parse(Float64, x["initial_value"])),
-            filter(x -> x["name"] ∈ params, vars))
-    return p
+function list_initial_conditions(xml, iv=find_iv(xml))
+    vars = find_initialized_variables(xml)
+    states = find_state_names(xml)
+    u0 = filter(x -> x["name"] ∈ states, vars)
+    map(x -> (create_var(x["name"], iv) => parse(Float64, x["initial_value"])), u0)
 end
 
-function list_u0(xml, iv=find_iv(xml))
-    vars = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
-    states = list_state_variables(xml)
-    u0 = map(x -> (create_var(x["name"], iv) => parse(Float64, x["initial_value"])),
-             filter(x -> x["name"] ∈ states, vars))
-    return u0
+function list_states(xml)
+    states = find_state_names(xml)
+    map(x -> (create_param(x)), states)
 end
 
 function list_substitution(xml, iv=find_iv(xml))
-    nodes = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
-    vars = map(x -> x["name"], nodes)
-    states = list_state_variables(xml)
-    Dict(create_var(x) => x ∈ states ? create_var(x, iv) : create_param(x) for x in vars)
+    # nodes = findall("//x:variable[@initial_value]", root(xml), ["x"=>cellml_ns])
+    # vars = map(x -> x["name"], nodes)
+    # states = find_state_names(xml)
+    # Dict(create_var(x) => x ∈ states ? create_var(x, iv) : create_param(x) for x in vars)
+    states = first.(list_initial_conditions(xml, iv))
+    params = first.(list_params(xml, iv))
+    u0_dict = Dict(create_var(operation(x).name) => x for x in states)
+    p_dict = Dict(create_var(x.name) => x for x in params)
+    union(u0_dict, p_dict)
 end
 
 function split_equations(eqs)
@@ -80,43 +71,50 @@ end
 function flatten_equations(eqs; level=1)
     eqs, alg = split_equations(eqs)
     s = Dict(a.lhs => a.rhs for a in alg)
+
     for i = 1:level
-        eqs = [substitute(eq.lhs, s) ~ substitute(eq.rhs, s) for eq in eqs]
+        alg = [eq.lhs ~ substitute(eq.rhs, s) for eq in alg]
     end
+
+    s = Dict(a.lhs => a.rhs for a in alg)
+    eqs = [eq.lhs ~ substitute(eq.rhs, s) for eq in eqs]
+
     return eqs
 end
 
-function generate_prob(xml, eqs, tspan)
+function read_cellml(xml::EzXML.Document; level=2)
+    ml = extract_mathml(xml)
+    eqs = vcat(parse_node.(ml)...)
+    eqs = flatten_equations(eqs; level=level)
     iv = find_iv(xml)
     s = list_substitution(xml, iv)
     eqs = [substitute(eq.lhs, s) ~ substitute(eq.rhs, s) for eq in eqs]
     sys = ODESystem(eqs, create_var(iv))
-    p = list_p(xml)
-    u0 = list_u0(xml, iv)
-    prob = ODEProblem(sys, u0, tspan, p)
-    return prob
+    return sys
 end
 
-# function generate_eval_string(xml, eqs)
-#     iv = find_iv(xml)
-#     params = "@parameters " * join(list_parameters(xml), " ") * " " * iv
-#     l = ["$x($iv)" for x in list_state_variables(xml)]
-#     vars = "@variables " * join(l, " ")
-#
-#     return params * ";" * vars * ";sys = ODESystem(" * string(eqs) * ");"
-# end
+# This is the fall-back option in case of breaking changes in
+# ModelingToolkit and/or SymbolicUtils
+function eval_string(xml::EzXML.Document)
+    iv = find_iv(xml)
+    p = list_params(xml, iv)
+    u0 = list_initial_conditions(xml, iv)
 
-using DifferentialEquations, Plots
+    states = [string(operation(first(x)).name) for x in u0]
+    params = [string(first(x).name) for x in p]
 
-function test()
-    # xml = readxml("models/lorenz.cellml.xml")
-    xml = readxml("models/beeler_reuter_1977.cellml.xml")
+    s = "@parameters " * join(params, " ") * " " * iv * ";"
+    s *= "iv_global = " * iv * ";"
+
+    d = "(" * iv * ") "
+    s *= "@variables " * join(states, d) * d * ";"
+
     ml = extract_mathml(xml)
-    eqs = parse_node.(ml)
-    eqs = vcat(eqs...)
-    eqs = flatten_equations(eqs; level=2)
-    prob = generate_prob(xml, eqs, (0, 5000.0))
-    sol = solve(prob, TRBDF2(), dtmax=0.5)
-    x = Array(sol)
-    plot(x[1,:])
+    eqs = vcat(parse_node.(ml)...)
+    s *= "eqs = " * string(eqs) * ";"
+
+    s *= "p_global = [" * join(string.(p), ",") *"];"
+    s *= "u0_global = [" * join([string(operation(first(x)).name) * " => " * string(last(x)) for x in u0], ", ") * "];"
+
+    return s
 end
