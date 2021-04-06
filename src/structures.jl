0d4ae234d834ed478c4efcfb3d4ad90282e966f1
Shahriar Iravanian <siravan@svtsim.com>
2021-04-06 15:24:18 -0400

    structures added

---
+++ src/structures.jl
@@ -0,0 +1,35 @@
+struct Var
+    comp::Symbol
+    var::Symbol
+end
+
+struct Component
+    name::Symbol
+    node::EzXML.Node
+end
+
+nameof(comp::Component) = comp.name
+
+struct Connection
+    comp_1::Symbol
+    comp_2::Symbol
+    vars::Array{Pair{Var}}
+end
+
+components(conn::Connection) = (conn.comp_1, conn.comp_2)
+variables(conn::Connection) = conn.vars
+
+struct Document
+    path::AbstractString
+    xmls::Array{EzXML.Document}
+    comps::Array{Component}
+    conns::Array{Connection}
+end
+
+components(doc::Document) = doc.comps
+connections(doc::Document) = doc.conns
+
+struct CellModel
+    doc::Document
+    sys::ODESystem
+end
