b10f90a89e9f68d4e7c105e67d4df1684c07f6b1
termi-official <termi-official@users.noreply.github.com>
2024-07-16 17:06:31 +0200

    Workaround for a possible TRBDF2 issue in one of the latest releases.

--- test/noble_1962.jl
+++ test/noble_1962.jl
@@ -3,7 +3,7 @@ ml = CellModel(path * "/../models/noble_1962/Noble_1962.cellml")
 
 prob = ODEProblem(ml, (0, 10000.0))
 sol1 = solve(prob, Euler(), dt = 0.01, saveat = 1.0)
-sol2 = solve(prob, TRBDF2(), dtmax = 0.5, saveat = 1.0)
+sol2 = solve(prob, ABDF2(), dtmax = 0.5, saveat = 1.0)
 V1 = map(x -> x[2], sol1.u)
 V2 = map(x -> x[2], sol2.u)
 err1 = sum(abs.(V1 .- V2)) / length(V1)
