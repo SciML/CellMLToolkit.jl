d40e7a2c6241853c3a6686a7f30aeb67b0379763
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-06-28 10:37:53 -0400

    QA: thorough EI ignore re-clean (drop stale incl via_owners, migrate non-public re-exports)
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -5,12 +5,6 @@ run_qa(
     explicit_imports = true,
     aqua_kwargs = (; ambiguities = (; recursive = false)),
     ei_kwargs = (;
-        all_qualified_accesses_via_owners = (;
-            ignore = (
-                :isparameter,   # owner ModelingToolkitBase, re-exported by ModelingToolkit
-                :unwrap,        # owner SymbolicUtils, re-exported by Symbolics
-            ),
-        ),
         all_qualified_accesses_are_public = (;
             ignore = (
                 :Document,      # EzXML (not declared public)
