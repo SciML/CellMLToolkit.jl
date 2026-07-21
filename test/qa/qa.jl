020787b0f0edf34e1c73d32993ec8e72f2fdfa84
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-21 00:40:01 -0400

    Migrate QA to SciMLTesting 2.4
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,8 +2,8 @@ using SciMLTesting, CellMLToolkit, Test
 
 run_qa(
     CellMLToolkit;
-    explicit_imports = true,
-    api_docs_kwargs = (; rendered = true, rendered_ignore = (:ODEProblem, :readxml)),
+    reexports_allow = (:ODEProblem, :readxml),
+    api_docs_kwargs = (; rendered_ignore = (:ODEProblem, :readxml)),
     aqua_kwargs = (; ambiguities = (; recursive = false)),
     ei_kwargs = (;
         all_qualified_accesses_are_public = (;
